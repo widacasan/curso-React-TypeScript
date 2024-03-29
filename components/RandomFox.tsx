076ffd6e8630bdcdaa2c7c3a2805e65fff6c4730
William David Camacho Sanchez <will@192.168.2.5>
2024-03-29 09:53:39 -0500

    add Lazy loading

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,17 +1,42 @@
-// Generates a random number between 1 and 122
+import { useRef, useEffect, useState } from "react";
 
+type Props = {
+  image: string;
+};
 
-type Props = { image: string}
+export function RandomFox({ image }: Props): JSX.Element {
+  const node = useRef<HTMLImageElement>(null);
+  const [src, setSrc] = useState(
+    "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+  );
 
-export function RandomFox({image}: Props): JSX.Element {
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting || !node.current) {
+          return;
+        }
 
+        setSrc(image);
+      });
+    });
+
+    if (node.current) {
+      observer.observe(node.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [image]);
 
   return (
     // eslint-disable-next-line @next/next/no-img-element
     <img
+      ref={node}
       width="320"
       height="auto"
-      src={image}
+      src={src}
       className="mx-auto rounded-md bg-gray-300"
     />
   );
